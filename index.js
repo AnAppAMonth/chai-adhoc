2330885a5defac96375084388fbe4ad121e444d8
Feng Qiu <feng@ban90.com>
2013-08-22 22:40:17 +0800

    Implement `addAssertion()`

---
+++ index.js
@@ -0,0 +1,112 @@
+
+var chai = require('chai'),
+    Assertion = chai.Assertion,
+    utils;
+
+chai.use(function (_chai, utilities) {
+    utils = utilities;
+});
+
+/**
+ * Function provided by the user in an `addAssertion` call.
+ *
+ * @callback addAssertionCallback
+ * @param {*} obj - Subject of the assertion
+ * @param {function} assert - Function to assert conditions or create assertion objects.
+ * @param {function} flag - Function to get and set flags.
+ * @returns {function} Function to be used for the new assertion.
+ */
+
+/**
+ * Getter provided by the user in an `addAssertion` call.
+ *
+ * @callback getterCallback
+ * @param {function} flag - Function to get and set flags.
+ */
+
+/**
+ * Callback provided by the user in an `wrapAssertion` call.
+ *
+ * @callback wrapAssertionCallback
+ * @param {*} obj - Subject of the assertion
+ * @param {function} assert - Function to assert conditions or create assertion objects.
+ * @param {function} flag - Function to get and set flags.
+ * @param {function} super - F
+ */
+
+var flag = function(name, value) {
+    if (arguments.length >= 2) {
+        return utils.flag(this, name, value);
+    } else {
+        return utils.flag(this, name);
+    }
+};
+
+var assert = function(expr, msg, neg_msg, expected, actual) {
+    if (neg_msg) {
+        // Should call this.assert().
+        return this.assert(expr, msg, neg_msg, expected, actual);
+    } else {
+        // Should create new `Assertion`.
+        var ass = new Assertion(expr, msg);
+        if (ass.flags === undefined) {
+            var that = this;
+            ass.flags = function() {
+                if (arguments.length === 0) {
+                    // Transfer all flags.
+                    utils.transferFlags(that, ass, false);
+                } else {
+                    // Only transfer the specified flags.
+                    for (var i = 0; i < arguments.length; i++) {
+                        utils.flag(ass, arguments[i], utils.flag(that, arguments[i]));
+                    }
+                }
+                return ass;
+            };
+        }
+        return ass;
+    }
+};
+
+/**
+ * Add a new assertion.
+ *
+ * @param {string} name - Name of the new assertion.
+ * @param {addAssertionCallback} func - Function to be called to define the assertion.
+ * @param {getterCallback} getter - Function to be called when the assertion is accessed
+ *                                  as a property. If set, a chainable method is created.
+ */
+function addAssertion(name, func, getter) {
+    var method, getterWrapper;
+
+    if (getter) {
+        method = 'addChainableMethod';
+        getterWrapper = function() {
+            return getter(flag.bind(this));
+        }
+    } else {
+        // Call the function to see how many parameters its returned function expects.
+        // This decides whether we should add a property or a method.
+        var test = func(1, assert, flag);
+        method = test.length ? 'addMethod' : 'addProperty';
+    }
+
+    return Assertion[method](name, function() {
+        var fn = func(this._obj, assert.bind(this), flag.bind(this));
+        return fn.apply(this, arguments);
+    }, getterWrapper);
+}
+
+function extendAssertion() {
+
+}
+
+function wrapAssertion() {
+
+}
+
+module.exports = {
+    addAssertion: addAssertion,
+    extendAssertion: extendAssertion,
+    wrapAssertion: wrapAssertion
+};
---
+++ test/example1.js
@@ -0,0 +1,138 @@
+
+/* global describe, it */
+/* jshint -W030 */
+var chai = require('../'),
+    expect = require('chai').expect;
+
+// Code to test
+function Model (type) {
+    this._type = type;
+    this._attrs = {};
+}
+
+Model.prototype.set = function (key, value) {
+    this._attrs[key] = value;
+};
+
+Model.prototype.get = function (key) {
+    return this._attrs[key];
+};
+
+var arthur = new Model('person');
+arthur.set('name', 'Arthur Dent');
+arthur.set('occupation', 'traveller');
+arthur.set('age', 27);
+
+// Define custom assertions
+
+// Property `model`
+chai.addAssertion('model', function(obj, assert, flag) {
+    return function() {
+        assert(
+            obj instanceof Model
+            , 'expected #{this} to be a Model'
+            , 'expected #{this} to not be a Model'
+        );
+    };
+});
+
+// Method `model2`
+var flagList = [];
+chai.addAssertion('model2', function(obj, assert, flag) {
+    return function(type) {
+        flagList.push(flag('negate'));
+        assert(obj, 'model type check').flags().to.be.instanceof(Model);
+        assert(obj, 'model type check').flags('negate').to.be.instanceof(Model);
+        assert(
+            obj._type === type
+            , "expected #{this} to be of type #{exp} but got #{act}"
+            , "expected #{this} to not be of type #{act}"
+            , type        // expected
+            , obj._type   // actual
+        );
+    };
+});
+
+// Chainable method `age`
+chai.addAssertion('age', function(obj, assert, flag) {
+    return function(n) {
+        // make sure we are working with a model
+        assert(obj).to.be.instanceof(Model);
+
+        // make sure we have an age and its a number
+        var age = obj.get('age');
+        assert(age).to.be.a('number');
+
+        // do our comparison
+        assert(
+            age === n
+            , "expected #{this} to have have #{exp} but got #{act}"
+            , "expected #{this} to not have age #{act}"
+            , n
+            , age
+        );
+    };
+}, function(flag) {
+    flag('model.age', true);
+});
+
+// Do the tests
+
+describe('Property model', function() {
+    it('should be successfully added', function() {
+        expect(expect(new Model())).to.have.property('model');
+        expect(function() {
+            expect(expect(1)).to.have.property('model');
+        }).to.throw();
+    });
+    it('should behave as expected', function() {
+        expect(arthur).to.be.a.model;
+        expect(1).to.not.be.a.model;
+        expect(function() {
+            expect(arthur).to.not.be.a.model;
+        }).to.throw();
+        expect(function() {
+            expect(1).to.be.a.model;
+        }).to.throw();
+    });
+});
+
+describe('Method model2', function() {
+    it('should be successfully added', function() {
+        expect(expect(new Model())).to.have.property('model2').that.is.a('function');
+        expect(function() {
+            expect(expect(1)).to.have.property('model2');
+        }).to.not.throw();
+    });
+    it('should behave as expected', function() {
+        expect(arthur).to.be.a.model2('person');
+        expect(1).to.not.be.a.model2('person');
+        expect(function() {
+            expect(arthur).to.not.be.a.model2('person');
+        }).to.throw();
+        expect(function() {
+            expect(1).to.be.a.model2('person');
+        }).to.throw();
+        expect(function() {
+            expect(arthur).to.be.a.model2('animal');
+        }).to.throw();
+        expect(function() {
+            expect(arthur).to.not.be.a.model2('animal');
+        }).to.throw();
+        expect(flagList).to.eql([undefined, true, true, undefined, undefined, true]);
+    });
+});
+
+describe('Chainable method age', function() {
+    it('should be successfully added', function() {
+        expect(expect(new Model())).to.have.property('age').that.is.a('function');
+        expect(function() {
+            expect(expect(1)).to.have.property('age');
+        }).to.not.throw();
+    });
+    it('should behave as expected', function() {
+        expect(arthur).to.have.age(27);
+        expect(arthur).to.have.age.above(17);
+        expect(arthur).to.not.have.age.below(18);
+    });
+});
