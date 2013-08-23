00047ee3712bcf03f9a4269f33a333c5ade42e15
Feng Qiu <feng@ban90.com>
2013-08-23 12:45:49 +0800

    Add tests for extending and wrapping an assertion many times

---
+++ test/multi.js
@@ -0,0 +1,113 @@
+
+/* global describe, it */
+/* jshint -W030 */
+var chai = require('../'),
+    expect = require('chai').expect;
+
+// Extend `below` 3 times
+chai.extendAssertion('below', function(obj, assert, flag) {
+    return function(n) {
+        if (obj >= 1000 && n >= 1000) {
+            // Require a difference of at least 100 for `below` to pass
+            assert(
+                obj+100 < n
+                , "expected #{this} to be below " + n
+                , "expected #{this} to not be below " + n
+            );
+            return true;
+        }
+        return false;
+    };
+});
+
+chai.extendAssertion('below', function(obj, assert, flag) {
+    return function(n) {
+        if (obj >= 5000 && n >= 5000) {
+            // Require a difference of at least 500 for `below` to pass
+            assert(
+                obj+500 < n
+                , "expected #{this} to be below " + n
+                , "expected #{this} to not be below " + n
+            );
+            return true;
+        }
+        return false;
+    };
+});
+
+chai.extendAssertion('below', function(obj, assert, flag) {
+    return function(n) {
+        if (obj >= 10000 && n >= 10000) {
+            // Require a difference of at least 1000 for `below` to pass
+            assert(
+                obj+1000 < n
+                , "expected #{this} to be below " + n
+                , "expected #{this} to not be below " + n
+            );
+            return true;
+        }
+        return false;
+    };
+});
+
+// Wrap `below` 3 times
+chai.wrapAssertion('below', function(obj, assert, flag) {
+    return function(error, n) {
+        if (error) {
+            error.message  = 'Prefix 1: ' + error.message;
+        }
+    };
+});
+
+chai.wrapAssertion('below', function(obj, assert, flag) {
+    return function(error, n) {
+        if (error) {
+            error.message  = 'Prefix 2: ' + error.message;
+        }
+    };
+});
+
+chai.wrapAssertion('below', function(obj, assert, flag) {
+    return function(error, n) {
+        if (error) {
+            error.message  = 'Prefix 3: ' + error.message;
+        }
+    };
+});
+
+describe('Multiple extensions to assertion below', function() {
+    it('should all take effect and in order', function() {
+        expect(15000).to.be.below(16001);
+        expect(15000).to.not.be.below(15999);
+        expect(7000).to.be.below(7501);
+        expect(7000).to.not.be.below(7499);
+        expect(2000).to.be.below(2101);
+        expect(2000).to.not.be.below(2099);
+        expect(function() {
+            expect(15000).to.not.be.below(16001);
+        }).to.throw(/^Prefix 3: Prefix 2: Prefix 1: /);
+        expect(function() {
+            expect(15000).to.be.below(15999);
+        }).to.throw(/^Prefix 3: Prefix 2: Prefix 1: /);
+        expect(function() {
+            expect(7000).to.not.be.below(7501);
+        }).to.throw(/^Prefix 3: Prefix 2: Prefix 1: /);
+        expect(function() {
+            expect(7000).to.be.below(7499);
+        }).to.throw(/^Prefix 3: Prefix 2: Prefix 1: /);
+        expect(function() {
+            expect(2000).to.not.be.below(2101);
+        }).to.throw(/^Prefix 3: Prefix 2: Prefix 1: /);
+        expect(function() {
+            expect(2000).to.be.below(2099);
+        }).to.throw(/^Prefix 3: Prefix 2: Prefix 1: /);
+    });
+});
+
+describe('Multiple wrappers of assertion below', function() {
+    it('should all take effect and in order', function() {
+        expect(function() {
+            expect(20).to.be.below(10);
+        }).to.throw(/^Prefix 3: Prefix 2: Prefix 1: /);
+    });
+});
